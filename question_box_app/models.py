1643c67ebfa4450fa8473dc497db5faa3cc51798
Will Warren <will.warren04@gmail.com>
2017-04-04 19:00:31 -0400

    made inital migrations and updated models

--- question_box_app/models.py
+++ question_box_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rank = models.Integer()
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextArea(max_length=1000)
+    tags = models.CharField(max_length=100) # how to parse tags?
+    rank = models.Integer()
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Answers(models.Model):
+    text = models.TextArea(max_length=10000)
+    rank = models.Integer()
+    comments = models.TextArea(max_length=10000)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_id = models.ForeignKey(Answers, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
