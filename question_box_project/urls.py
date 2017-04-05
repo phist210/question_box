06c70400a1010f1b39a58603d5f83f3d193e547a
Will Warren <will.warren04@gmail.com>
2017-04-05 14:53:41 -0400

    built user login modal. testing now

--- question_box_project/urls.py
+++ question_box_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name="login"),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^question_box_app/', include('question_box_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
