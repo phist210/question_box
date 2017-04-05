31e7d5966e7c63c6859c0eea7fe774c73e031e21
Josh Friese <Friese@Friese.local>
2017-04-05 14:17:11 -0400

    connected to templates

--- question_box_app/models.py
+++ question_box_app/models.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rank = models.Integer()
+    rank = models.IntegerField()
 
 
 class Question(models.Model):
     title = models.CharField(max_length=100)
-    text = models.TextArea(max_length=1000)
+    text = models.TextField(max_length=1000)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class Answer(models.Model):
-    text = models.TextArea(max_length=10000)
+    text = models.TextField(max_length=10000)
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,27 +26,27 @@ class Answer(models.Model):
 class QuestionComment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.TextArea(max_length=10000)
+    text = models.TextField(max_length=10000)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class AnswerComment(models.Model):
-    text = models.TextArea(max_length=10000)
-    created = models.DataTimeField(auto_now_add=True)
+    text = models.TextField(max_length=10000)
+    created = models.DateTimeField(auto_now_add=True)
     answer_id = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    user_id = models.ForgeinKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class AnswerVote(models.Model):
     id_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.Integer()
+    score = models.IntegerField()
 
 
 class QuestionVote(models.Model):
     id_question = models.ForeignKey(Question, on_delete=models.CASCADE)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.Integer()
+    score = models.IntegerField()
 
 
 class Tag(models.Model):
--- question_box_app/views.py
+++ question_box_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(response, 'question_box_app/index.html')
+    return render(request, 'question_box_app/index.html')
