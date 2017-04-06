235707be6d9bd37b89d9d150ddd54fedaa1b4c76
Will Warren <will.warren04@gmail.com>
2017-04-06 15:56:57 -0400

    updated

--- question_box_app/static/js/askq.js
+++ question_box_app/static/js/askq.js
@@ -1,1 +1,19 @@
-// 
+//form to ask questions and post them via ajax
+
+$('#submit').click(function(event) {
+    event.preventDefault();
+    console.log("hoi");
+
+    $.ajax({
+        type:'POST',
+        url: 'api/question/',
+        success: function(result) {
+            alert("Question added!")
+        }
+
+
+
+
+    })
+
+});
