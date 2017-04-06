e9e432fb0d72d8e443185d68110a31668e811469
Josh Friese <Friese@Friese.local>
2017-04-06 17:20:49 -0400

    handling merge conflicts from askform

--- question_box_app/static/js/base.js
+++ question_box_app/static/js/base.js
@@ -2,17 +2,13 @@ function getQuestions() {
   var questionApi = "/api/question/";
   $.ajax({url: questionApi, success: function(result) {
     var questionLength = result.length;
-    console.log(result);
-    console.log(questionLength);
     for(var i = 0; (i + 1) <= questionLength; i++){
       var questionTitle = result[i].title;
       var questionText = result[i].text;
-      console.log(result[i].title);
       var questionID = i + 1;
-      console.log(questionID);
         $('div.block').append('<div class=question id=' + questionID + '>' + "<a href=/question/" + questionID + ">" + questionTitle + "</a>"  + '<br>' + questionText + "</div>");
-        };
-      }
-    });
+      };
+    }
+  });
 }
 getQuestions();
