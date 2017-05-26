64052a02ca3a3d2f21b54f9e3e2f9cf7b5c00346
Will Warren <will.warren04@gmail.com>
2017-05-25 21:35:02 -0400

    manual garbage collection

--- question_box_app/static/js/new_answer.js
+++
@@ -1,6 +0,0 @@
-//new answer file
-//will submit answers, show ans text, author, time, score, and commentanswer
-//three models, probably should be one next time, but a promise is useful here.
-//ajax gets from Answer, VoteAnswer, AnswerComments models
-
-//get the page information
